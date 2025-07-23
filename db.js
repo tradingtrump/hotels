526417625c6efb8b215cbc8bc96069a69f9b307d
Alexandria2109 <117550186+Alexandria2109@users.noreply.github.com>
2025-07-23 09:59:14 -0500

    update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "lcy011209",
+  password: //ENTER YOUR PASSWORD//
   database: "hotelhub"
 });
 db.connect(err => {
